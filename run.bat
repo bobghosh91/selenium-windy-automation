d335539b9c24404b0d5984c2d5ffc0eb52fb8259
Bob Ghosh <bobghosh91@gmail.com>
2024-10-11 12:51:39 +0530

    added jenkins file

--- run.bat
+++ run.bat
@@ -11,12 +11,13 @@ REM Run pytest with the HTML report option including the timestamp
 @REM python -m pytest -m "sanity or regression" --reruns 2 --browser_name chrome tests\ --html=reports/report_%timestamp%.html
 
 REM Run pytest in headless mode
-@REM python -m pytest -m "sanity or regression" --reruns 2 --browser_name --headless chrome tests\ --html=reports/report_%timestamp%.html
+@REM python -m pytest -m "sanity or regression" --reruns 2 --browser_name chrome --headless tests\ --html=reports/report_%timestamp%.html
 
-REM Run pytest headed
+REM Run pytest headed mode
 python -m pytest -m "sanity or regression" --reruns 2 --browser_name chrome tests\ --html=reports/report_%timestamp%.html
-@REM python -m pytest -n auto --browser_name chrome tests\
-@REM python -m pytest --browser_name chrome tests\
+
+REM Run pytest in parallel mode with pytest-xdist library
+@REM python -m pytest -m "sanity or regression" -n auto --reruns 2 --browser_name chrome tests\ --html=reports/report_%timestamp%.html
 
 REM generate allure report single file
 allure generate --single-file ./reports/allure-results -o ./reports/allure-reports --clean
